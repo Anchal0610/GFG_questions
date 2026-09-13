0f7d2d9599299f3f488f589bafdf4b4c59cae46a
Anchal Gupta <anchalgupta0602@gmail.com>
2026-09-14 02:39:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Largest/kth-largest.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int kthLargest(int arr[], int k) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ele : arr){
+            pq.add(ele);
+            if(pq.size() > k){
+                pq.remove();
+            }
+        }
+        return pq.peek();
+    }
+}
